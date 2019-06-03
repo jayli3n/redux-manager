a1329763811fb3864c809f663dc341dfe797a59e
Jay Li <jayli3n@gmail.com>
2019-06-03 18:57:57 +0800

    added firebase

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import {
+	apiKey,
+	authDomain,
+	databaseURL,
+	projectId,
+	storageBucket,
+	messagingSenderId,
+	appId
+} from 'react-native-dotenv';
 import reducers from './reducers';
 
 class App extends Component {
+	componentWillMount() {
+		// Initialize Firebase
+		const firebaseConfig = {
+			apiKey,
+			authDomain,
+			databaseURL,
+			projectId,
+			storageBucket,
+			messagingSenderId,
+			appId
+		};
+		firebase.initializeApp(firebaseConfig);
+	}
+
 	render() {
 		return (
 			<Provider store={createStore(reducers)}>
