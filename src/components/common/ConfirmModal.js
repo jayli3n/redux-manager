d0edca9792885b6b415102c72ab63565124c26b6
Jay Li <jayli3n@gmail.com>
2019-06-05 20:00:30 +0800

    fix bracket

--- src/components/common/ConfirmModal.js
+++ src/components/common/ConfirmModal.js
@@ -40,7 +40,7 @@ const styles = {
 		lineHeight: 40
 	},
 	containerStyle: {
-		backgroundColor: 'rgba(0,0,0,0.75',
+		backgroundColor: 'rgba(0,0,0,0.75)',
 		position: 'relative',
 		flex: 1,
 		justifyContent: 'center'
